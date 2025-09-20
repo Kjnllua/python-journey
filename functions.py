74b213ac7b3176867735193e054a0cc01123e6af
Kjnllua <kjnyuamax@gmail.com>
2025-09-20 13:19:43 +0300

    Added question 13

--- functions.py
+++ functions.py
@@ -133,3 +133,11 @@ def prime_numbers(n):
         else:
             is_prime.append(num)
     return is_prime
+
+# Question 13
+"""
+See if you can write a function that does the same thing as the shell command !head.
+It should take as arguments the name of a file to read, the number of lines to read,
+and the name of the file to write the lines into. If the third parameter is None, it
+should display the lines rather than write them to a file.
+"""
